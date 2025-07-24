4b935c19ff076093d246d7151f223205e11928f6
NandaKumarMatha <nandakumarmatha98@gmail.com>
2025-07-25 02:58:41 +0530

    refactor: update Footer component copyright format and enhance hero section styles for better responsiveness

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <Box component="footer" py={3} textAlign="center" bgcolor="secondary.main" color="white">
       <Typography variant="body1">
-        &copy; {new Date().getFullYear()} <a href="#home">M. N. Kumar</a>. All rights reserved.
+        &copy; {new Date().getFullYear()} <a href="#home">M.N.Kumar</a>. All rights reserved.
       </Typography>
     </Box>
   );
